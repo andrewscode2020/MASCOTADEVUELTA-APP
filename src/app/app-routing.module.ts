51e0e936230776369d6a0369e8bb583d4c6f4c06
andrewscode2020 <greatandres@gmail.com>
2020-11-24 22:06:52 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,76 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Importamos las rutas:
+import { BuscaTuMascotaComponent } from './components/busca-tu-mascota/busca-tu-mascota.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: '', // http://localhost:4200
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',  // http://localhost:4200/home
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+// import { BuscaTuMascotaComponent } from './componentes/busca-tu-mascota/busca-tu-mascota.component';
+// import { CasosFelicesComponent } from './componentes/casos-felices/casos-felices.component';
+// import { HomeComponent } from './componentes/home/home.component';
+// import { IngresaComponent } from './componentes/ingresa/ingresa.component';
+// import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
+// import { RegistrateComponent } from './componentes/registrate/registrate.component';
+// import { ReportaUnaMascotaComponent } from './componentes/reporta-una-mascota/reporta-una-mascota.component';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     redirectTo: 'home',
+//     pathMatch: 'full'
+//   },
+//   {
+//     path: 'home',
+//     component: HomeComponent
+//   },
+//   {
+//     path: 'buscaTuMascota',
+//     component: BuscaTuMascotaComponent
+//   },
+//     {
+//     path: 'reportaUnaMascota',
+//     component: ReportaUnaMascotaComponent
+//   },
+//   {
+//     path: 'casosFelices',
+//     component: CasosFelicesComponent
+//   },
+//     {
+//     path: 'ingresa',
+//     component: IngresaComponent
+//   },
+//   {
+//     path: 'registrate',
+//     component: RegistrateComponent
+//   },
+//   {
+//     path: '**',
+//     component: PaginaNoEncontradaComponent
+//   }
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import { AppRoutingModule } from './app-routing.module';
+// import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BuscaTuMascotaComponent } from './components/busca-tu-mascota/busca-tu-mascota.component';
+import { ReportaUnaMascotaComponent } from './components/reporta-una-mascota/reporta-una-mascota.component';
+import { CasosFelicesComponent } from './components/casos-felices/casos-felices.component';
+import { IngresaComponent } from './components/ingresa/ingresa.component';
+import { RegistrateComponent } from './components/registrate/registrate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BuscaTuMascotaComponent,
+    ReportaUnaMascotaComponent,
+    CasosFelicesComponent,
+    IngresaComponent,
+    RegistrateComponent
   ],
   imports: [
     BrowserModule,
