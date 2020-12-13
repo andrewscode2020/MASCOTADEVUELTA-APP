6fbb6141fa66f41ff0c7acac1240656bc327dc08
pilitaz <jennyp.gomezb@hotmail.com>
2020-12-13 17:26:49 -0500

    formulario de registro de mascota con selects desde servicio

--- src/app/components/reporta-una-mascota/reporta-una-mascota.component.ts
+++ src/app/components/reporta-una-mascota/reporta-una-mascota.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PetReportService } from 'src/app/services/pet-report.service';
+import { Breed } from 'src/app/interfaces/breed';
+import { BreedService } from "../../services/breed.service"
+import { PetType } from 'src/app/interfaces/pet-type';
+import { PetTypeService } from '../../services/pet-type.service';
+import { City } from 'src/app/interfaces/city';
+import { CityService } from '../../services/city.service';
+import { Country } from 'src/app/interfaces/country';
+import { CountriesService } from "../../services/countries.service";
+import { State } from 'src/app/interfaces/state'
+import { StateService } from "src/app/services/state.service"
 
 @Component({
   selector: 'app-reporta-una-mascota',
@@ -9,39 +19,45 @@ import { PetReportService } from 'src/app/services/pet-report.service';
 })
 export class ReportaUnaMascotaComponent {
   newPetReport: FormGroup;
+  genders: Array<Object> = [{ text: "Macho", value: "Macho" }, { text: "Hembra", value: "Hembra" }];
+  petTypes: Array<PetType> = [];
+  breeds: Array<Breed> = [];
+  countries: Array<Country> = [];
+  states: Array<State> = [];
+  cities: Array<City> = [];
 
   constructor(
     private formBuilder: FormBuilder,
-    private petReportService: PetReportService
-  ) { 
+    private petReportService: PetReportService, private petTypeService: PetTypeService, private breedService: BreedService, private cityService: CityService, private countriesService: CountriesService, private stateService: StateService
+  ) {
     this.newPetReport = this.formBuilder.group({
-        reportType: '',
-        petType: '',
-        name: '',
-        breed: '',
-        gender: '',
-        age: '',
-        size: '',
-        castrated: '',
-        chip: true,
-        chipNumber: '',
-        petPic: [],
-        additionalFeatures: '',
-        eventDate: '',
-        eventDescription: '',
-        country: '',
-        state: '',
-        city: '',
-        neighborhood: '',
-        reward: true,
-        rewardValue: 0,
-        comments: [],
-        user: {type: '', required: true },
-        publicPhone: false,
-        timestamp: '',
-        closed: true,
-        successful: true,
-        endNote: ''
+      reportType: '',
+      petType: '',
+      name: '',
+      breed: '',
+      gender: '',
+      age: '',
+      size: '',
+      castrated: '',
+      chip: true,
+      chipNumber: '',
+      petPic: [],
+      additionalFeatures: '',
+      eventDate: '',
+      eventDescription: '',
+      country: '',
+      state: '',
+      city: '',
+      neighborhood: '',
+      reward: true,
+      rewardValue: 0,
+      comments: [],
+      user: { type: '', required: true },
+      publicPhone: false,
+      timestamp: '',
+      closed: true,
+      successful: true,
+      endNote: ''
     })
   }
 
@@ -73,17 +89,84 @@ export class ReportaUnaMascotaComponent {
       // petData.append('city', formObj.city);
       // const imageInput: HTMLElement = document.querySelector('input#customFile');
       // petData.append('petPic', imageInput.files[0]);
-      
+
       this.petReportService.createNewReport(petData)
+        .subscribe(
+          (res) => {
+            console.log('Mascota registrada exitosamente', res)
+          },
+          (err) => {
+            console.error('Error al registrar mascota: ', err)
+          }
+        )
+    }
+  }
+
+  ngOnInit(): void {
+    this.getPetTypesList();
+    this.getCountries();
+  }
+
+  getPetTypesList() {
+    this.petTypeService.getPetTypes()
       .subscribe(
-        (res) => {
-          console.log('Mascota registrada exitosamente', res)
+        (petTypesList) => {
+          this.petTypes = petTypesList;
         },
-        (err) => {
-          console.error('Error al registrar mascota: ', err)
+        (error) => {
+          console.error('No se pudo cargar los tipos de mascota', error)
+
         }
       )
-    }
+  }
+
+  getBreedsForPetType() {
+    this.breedService.getBreedByPetType(this.newPetReport.value.petType)
+      .subscribe(
+        (breedsList) => {
+          this.breeds = breedsList;
+        },
+        (error) => {
+          console.error('No se pudo cargar las razas de perro', error)
+
+        }
+      )
+  }
+
+  getCountries() {
+    this.countriesService.getCountries()
+      .subscribe(
+        (countriesList) => {
+          this.countries = countriesList;
+        },
+        (error) => {
+          console.error('No se pudo cargar las ciudades', error)
+        }
+      )
+  }
+
+  getStates() {
+    this.stateService.getStatesByCountry(this.newPetReport.value.country)
+      .subscribe(
+        (statesList) => {
+          this.states = statesList;
+        },
+        (error) => {
+          console.error('No se pudo cargar los estados', error)
+        }
+      )
+  }
+
+  getCities() {
+    this.cityService.getCitiesByState(this.newPetReport.value.state)
+      .subscribe(
+        (citiesList) => {
+          this.cities = citiesList;
+        },
+        (error) => {
+          console.error('No se pudo cargar las ciudades', error)
+        }
+      )
   }
 
 }
